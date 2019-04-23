bf4819aa669e8d4c4e3e9e23881b303485994c2b
Okai-Kuwsh <47816668+Okai-Kuwsh@users.noreply.github.com>
2019-04-23 19:09:05 +0100

    Yay
    I did it!

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Kuwsh</title>
+    <title>Kuwsh Okai</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <style>
         /* .navbar-text {
